5dc39ee93e7f6701a3ae64e37bc5fbae23bb626b
Laura Galbraith <lagalbra@microsoft.com>
2016-09-13 21:18:30 +0000

    Moving changes from Microsoft branch to fs_mysql branch

--- source/code/plugins/filter_mysql_logs.rb
+++ source/code/plugins/filter_mysql_logs.rb
@@ -5,6 +5,8 @@ module Fluent
     def initialize
       super
       require 'socket'
+      require 'date'
+      require_relative 'oms_common'
     end
 
     def configure(conf)
@@ -20,10 +22,32 @@ module Fluent
     end
 
     def filter(tag, time, record)
-      record["ResourceName"] = 'Mysql'
-      record["Computer"] = IPSocket.getaddress(Socket.gethostname)
+      record["ResourceName"] = 'MySQL'
+      record["Computer"] = OMS::Common.get_hostname
       record["ResourceId"] = Socket.gethostname
-      record["ResourceType"] = tag.split('.')[3]
+      resource_type = tag.split('.')[3]
+      record["ResourceType"] = resource_type
+
+      if resource_type == "SlowQuery"
+        # Convert type of decimal fields
+        record["QueryTime"] = record["QueryTime"].to_f
+        record["LockTime"] = record["LockTime"].to_f
+        record["RowsSent"] = record["RowsSent"].to_i
+        record["RowsExamined"] = record["RowsExamined"].to_i
+        # Convert timestamp to OMS style
+        timestamp = record["Timestamp"].to_i
+        record["Timestamp"] = OMS::Common.format_time(timestamp)
+      elsif resource_type == "General"
+        # Convert InitTime to OMS style
+        init_time = record["InitTime"]
+        if init_time != nil
+          record["InitTime"] = DateTime.parse(init_time).strftime("%FT%H:%M:%S.%3NZ")
+        end
+      elsif resource_type == "Error"
+        # Convert timestamp to OMS style
+        timestamp = record["Timestamp"]
+        record["Timestamp"] = DateTime.parse(timestamp).strftime("%FT%H:%M:%S.%3NZ")
+      end
 
       record
     end
